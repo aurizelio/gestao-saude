aa000c03ae81d6ce8686edc432c0f667e651c477
Francisco <francisco.souza63@etec.sp.gov.br>
2023-08-27 21:01:08 -0300

    v1

---
+++ src/main/java/com/multsaude/gestaosaude/GestaoSaudeApplication.java
@@ -0,0 +1,13 @@
+package com.multsaude.gestaosaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoSaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoSaudeApplication.class, args);
+	}
+
+}
