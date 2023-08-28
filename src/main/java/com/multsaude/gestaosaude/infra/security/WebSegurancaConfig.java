156941ab1eeb570f7929b390bb502670e5f1d5ab
Francisco <francisco.souza63@etec.sp.gov.br>
2023-08-27 22:14:13 -0300

    V1

--- src/main/java/com/multsaude/gestaosaude/infra/security/WebSegurancaConfig.java
+++ src/main/java/com/multsaude/gestaosaude/infra/security/WebSegurancaConfig.java
@@ -4,21 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSegurancaConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    @Bean
+
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
     }
