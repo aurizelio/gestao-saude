719403359a55e5548a3ec3dc3b33d6112d8113c7
Francisco <francisco.souza63@etec.sp.gov.br>
2023-08-27 22:06:31 -0300

    V1

--- src/main/java/com/multsaude/gestaosaude/infra/security/WebSegurancaConfig.java
+++ src/main/java/com/multsaude/gestaosaude/infra/security/WebSegurancaConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableWebSecurity
 public class WebSegurancaConfig extends WebSecurityConfigurerAdapter {
 
     @Override
